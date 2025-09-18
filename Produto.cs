5668d0c682ffdab4dfbcd3181793d34787d3028d
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 17:49:44 -0300

    first commit

---
+++ Produto.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+
+namespace ClasseAtributosMetodos2ConstrutorSobrecargaThisEncapsulamento
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+            _quantidade = 10;
+        }
+
+        public Produto(string nome, double preco) : this()
+        {
+            _nome = nome;
+            _preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            _quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public void SetPreco(double preco)
+        {
+            _preco = preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $"
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ClasseAtributosMetodos2ConstrutorSobrecargaThisEncapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Produto p = new Produto(nome, preco);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(quantidade);
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(quantidade);
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
